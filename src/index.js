74b7080ca46b7f601a980409608bf0171aabb3a5
sumitdevda2001 <sumitdevda2001@gmail.com>
2023-04-27 11:00:44 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
+import { UserProvider } from "./contexts/user.context";
+import { CategoriesProvider } from "./contexts/categories.context";
+import { CartProvider } from "./contexts/cart.context";
+
+
+import "./index.scss";
+
+
+
+
+const rootElement = document.getElementById("root");
+render(
+  <React.StrictMode>
+    <BrowserRouter>
+     <UserProvider>  
+     <CategoriesProvider>
+     <CartProvider>
+     <App />
+     </CartProvider>
+     </CategoriesProvider>
+     </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  rootElement
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,51 @@
+import { Fragment, useContext } from "react";
+import { Outlet, } from "react-router-dom";
+
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
+
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+
+import{NavigationContainer,NavLinks,NavLink,LogoContainer}from "./navigation.styles";
+
+const Navigation = () => {
+  const { currentUser,setCurrentUser } = useContext(UserContext);
+  const {isCartOpen} = useContext(CartContext)
+
+  const signOutHandler = async () => {
+     await signOutUser();
+     setCurrentUser(null);
+  };
+  return (
+    <Fragment>
+      <NavigationContainer>
+        <LogoContainer to="/">
+          <CrwnLogo className="logo" />
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to="/shop">
+            SHOP
+          </NavLink>
+          {currentUser ? (
+            <NavLink as='span' onClick={signOutHandler}>
+              SIGN OUT
+            </NavLink>
+          ) : (
+            <NavLink to="/auth">
+              SIGN IN
+            </NavLink>
+          )}
+          <CartIcon />
+        </NavLinks>
+        {isCartOpen && <CartDropdown />}
+      </NavigationContainer>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
